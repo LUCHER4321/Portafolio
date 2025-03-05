38ee3e06c723cce66ba8ee57dce1d612fbbdbe30
LUCHER4321 <lucher4321@gmail.com>
2025-03-05 02:49:07 -0300

    Create ImageLink.tsx

---
+++ src/scripts/ImageLink.tsx
@@ -0,0 +1,15 @@
+const defaultImage = "https://cdn-icons-png.flaticon.com/512/25/25284.png";
+
+interface ImageLinkProps {
+    link: string;
+    image?: string;
+    height?: number;
+}
+
+export const ImageLink = ({link, image = undefined, height = 128}: ImageLinkProps) => {
+    return (
+        <a href={link} target="_blank">
+            <img height={height} src={image ?? defaultImage}/>
+        </a>
+    );
+}
