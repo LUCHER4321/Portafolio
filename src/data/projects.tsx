45ebc57a7c40367caf090f5d1dca95a28693d5fb
LUCHER4321 <lucher4321@gmail.com>
2025-03-08 17:18:34 -0300

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -9,5 +9,11 @@ export const projects = [
     ["Lista de Tareas", "https://github.com/LUCHER4321/Lista-Tareas", "https://lucher4321.github.io/Lista-Tareas"],
     ["Calculadora de Propinas", "https://github.com/LUCHER4321/Calculadora-Propinas", "https://lucher4321.github.io/Calculadora-Propinas"],
     ["Postulación a RECEMED", "https://github.com/LUCHER4321/RECEMED"],
-    ["Power BI Netflix", "https://github.com/LUCHER4321/Power-BI-Netflix", "https://aka.ms/AAuyv0k", "https://1000logos.net/wp-content/uploads/2022/12/Power-BI-Logo.png"]
-].map(l => new Project(l[0], l[1], l.length > 2 ? l[2] : undefined, l.length > 3 ? l[3] : undefined));
+    ["Power BI Netflix", "https://github.com/LUCHER4321/Power-BI-Netflix", "https://aka.ms/AAuyv0k", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png", "Power BI"],
+    ["Noble Corral", "https://github.com/LUCHER4321/Noble-Corral", undefined, undefined, "Power BI", "R"],
+    ["Valoraciones Steam", "https://github.com/LUCHER4321/Valoraciones-Steam", undefined, undefined, "R"],
+    ["Edificios Ecotect", "https://github.com/LUCHER4321/Edificios-Ecotect", undefined, undefined, "R"],
+    ["Pacientes ACV", "https://github.com/LUCHER4321/Pacientes-ACV", undefined, undefined, "R"],
+    ["Accidentes Cerebro Vasculares", "https://github.com/LUCHER4321/ACV", undefined, undefined, "R"],
+    ["Informe Mundial sobre la Felicidad", "https://github.com/LUCHER4321/Informe-Felicidad", undefined, undefined, "R"],
+].map(l => new Project(l[0] ?? "", l[1] ?? "", l.length > 2 ? l[2] : undefined, l.length > 3 ? l[3] : undefined, ...(l.length > 4 ? l.slice(4).map(s => s ?? "") : [])));
