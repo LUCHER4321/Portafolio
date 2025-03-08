1aaad2e8a450f4807933dabb7153d1fed28a3586
LUCHER4321 <lucher4321@gmail.com>
2025-03-07 23:32:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,22 @@ import { PersonalLink } from './scripts/PersonalLink';
 import { links } from './data/links';
 
 function App() {
-  const collapse = {borderCollapse: "collapse", width: "100%"} as React.CSSProperties;
-  const border = {border: "1px solid"} as React.CSSProperties;
-  const start = {textAlign: "start"} as React.CSSProperties;
   return (
     <>
       <h1>Portafolio Luciano Hernández</h1>
-      <h2 style={start}>Enlaces</h2>
+      <h2 className="text-start text-2xl font-bold py-5">Enlaces</h2>
       <PersonalLink.Table
         links={links}
         height={24}
-        tdStyle={start}
+        tdStyle="text-start py-0.5"
       />
-      <h2 style={start}>Proyectos</h2>
+      <h2 className="text-start text-2xl font-bold py-5">Proyectos</h2>
       <Project.Table
         projects={projects}
         height={50}
-        style={collapse}
-        thStyle={border}
-        tdStyle={border}
+        style="border-collapse w-full"
+        thStyle="border border-solid"
+        tdStyle="border border-solid p-2.5"
       />
     </>
   )
