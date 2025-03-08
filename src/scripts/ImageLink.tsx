eaea419cf44f337c327a3fada62bd14ca75ca6e5
LUCHER4321 <lucher4321@gmail.com>
2025-03-08 17:40:50 -0300

    Update ImageLink.tsx

--- src/scripts/ImageLink.tsx
+++ src/scripts/ImageLink.tsx
@@ -9,7 +9,7 @@ interface ImageLinkProps {
 export const ImageLink = ({link, image = undefined, height = 128}: ImageLinkProps) => {
     return (
         <a href={link} target="_blank" className="flex justify-center">
-            <img style={{height: height}} src={image ?? defaultImage}/>
+            <img style={{height: height, aspectRatio: '1/1'}} src={image ?? defaultImage}/>
         </a>
     );
 }
