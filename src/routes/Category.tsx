b5a5cdb0ab1b9771e1c629d371e52217dff0ed61
LUCHER4321 <lucher4321@gmail.com>
2025-04-08 15:31:19 -0400

    Create Category.tsx

---
+++ src/routes/Category.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { useSearchParams } from "react-router-dom";
+import { NavBar } from "../scripts/NavBar";
+import { Project } from "../scripts/Project";
+import { projects } from "../data/projects";
+import { codeText } from "../scripts/translate";
+import { categories } from "../data/categories";
+import { Category } from "../scripts/Category";
+
+export const CategoryPage = () => {
+    const [language, setLanguage] = useState("spanish");
+    const [searchParams] = useSearchParams();
+    const cat = searchParams.get("cat") || "";
+    const category = categories.find(c => c.id === cat) ?? new Category(cat, cat);
+    return (
+        <>
+            <NavBar language={language} setLanguage={setLanguage}/>
+            <h1>{codeText("ttl02", language, [category.name.get(language) ?? ""])}</h1>
+            <div className="flex justify-center mt-4">
+                <Project.Table
+                    projects={projects.filter(p => p.categories.includes(category))}
+                    language={language}
+                    height={50}
+                    lanSize={20}
+                    className="border-collapse"
+                    thClassName="border border-solid px-2"
+                    tdClassName="border border-solid p-2.5"
+                />
+            </div>
+        </>
+    )
+}
