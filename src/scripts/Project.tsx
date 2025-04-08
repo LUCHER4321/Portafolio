4ddd03c6548bbf8753ae8926ff320a5cc377f799
LUCHER4321 <lucher4321@gmail.com>
2025-04-08 15:31:01 -0400

    Update Project.tsx

--- src/scripts/Project.tsx
+++ src/scripts/Project.tsx
@@ -3,9 +3,12 @@ import { ImageLink } from "./ImageLink";
 import { Language } from "./Language";
 import { languages } from "../data/languages";
 import { codeText } from "./translate";
+import { Category } from "./Category";
+import { categories } from "../data/categories";
 
 export class Project {
     name: Map<string,string>;
+    categories: Category[];
     repository: string;
     website?: string;
     icon?: string;
@@ -13,25 +16,28 @@ export class Project {
 
     private static GitHubLogo = "https://logo.clearbit.com/github.com";
 
-    constructor(name: string, repository: string, website: string | undefined = undefined, icon: string | undefined = undefined, ...languages: string[]) {
+    constructor(name: string, categoryIDs: string[], repository: string, website: string | undefined = undefined, icon: string | undefined = undefined, ...languages: string[]) {
         this.name = new Map(name.split(";").map(s => [s.split(":")[0], s.split(":")[1]]));
+        this.categories = categories.filter(c => categoryIDs.includes(c.id));
         this.repository = repository;
         this.website = website;
         this.icon = icon;
         this.languages = languages;
     }
 
-    static Table({projects, className, height, thClassName, tdClassName, languageFilter, lanHeight, language}: tableProps) {
+    static Table({projects, className, height, thClassName, tdClassName, languageFilter, lanSize, language}: tableProps) {
+        const filteredProjects = languageFilter ? projects.filter(p => {
+            for(const l of languageFilter){
+                if(p.languages.includes(l.name)) return true;
+            }
+            return false;
+        }) : projects
+        const web = filteredProjects.filter(p => p.website).length > 0;
         return(
             <CustomTable
-                headers={["00", "01", "02", "03"].map(n => codeText("hdr" + n, language) ?? "")}
-                data={languageFilter ? projects.filter(p => {
-                    for(const l of languageFilter){
-                        if(p.languages.includes(l.name)) return true;
-                    }
-                    return false;
-                }) : projects}
-                row={p => p ? [
+                headers={["00", "01", "02"].concat(web ? ["03"] : []).map(n => codeText("hdr" + n, language) ?? "")}
+                data={filteredProjects}
+                row={p => ([
                     p.name.get(language) ?? [...p.name.values()][0],
                     <ImageLink
                         link={p.repository}
@@ -41,14 +47,9 @@ export class Project {
                     <Language.List
                         languages={languages.filter(l => p.languages.includes(l.name))}
                         className="flex flex-wrap"
-                        height={lanHeight}
-                    />,
-                    p.website && <ImageLink
-                        link={p.website}
-                        image={p.icon}
-                        height={height}
+                        size={lanSize}
                     />
-                ] : []}
+                ] as any[]).concat(web ? [p.website && <ImageLink link={p.website} image={p.icon} height={height}/>] : [])}
                 className={className}
                 thClassName={thClassName}
                 tdClassName={tdClassName}
@@ -64,6 +65,6 @@ interface tableProps {
     thClassName?: string;
     tdClassName?: string;
     languageFilter?: Language[];
-    lanHeight?: number;
+    lanSize?: number;
     language: string;
 }
