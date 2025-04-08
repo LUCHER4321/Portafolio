7af1e198f4bcb0aa5f1eef877bb2f7c750225588
LUCHER4321 <lucher4321@gmail.com>
2025-04-08 15:31:24 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { LanguagePage } from './routes/Language.tsx'
+import { CategoryPage } from './routes/Category.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Portfolio" element={<App/>}/>
+        <Route path="/Portfolio/Language" element={<LanguagePage/>}/>
+        <Route path="/Portfolio/Category" element={<CategoryPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
