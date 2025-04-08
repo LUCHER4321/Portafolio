8332a0aa42359648873a9fda4ed819b0fb23756b
LUCHER4321 <lucher4321@gmail.com>
2025-04-08 15:29:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,90 +2,56 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-import { projects } from './data/projects'
-import { Project } from './scripts/Project'
 import { PersonalLink } from './scripts/PersonalLink';
 import { links } from './data/links';
 import { Language } from './scripts/Language';
 import { languages } from './data/languages';
-import { codeText, getLanguageOptions } from './scripts/translate';
+import { codeText } from './scripts/translate';
+import { NavBar } from './scripts/NavBar';
+import { Category } from './scripts/Category';
+import { categories } from './data/categories';
 
 function App() {
   const [language, setLanguage] = useState("spanish");
-  const [languageFilter, setLanguageFilter] = useState(languages);
-  const trLanguages = getLanguageOptions();
-
-  const addLanguage = (l: Language) => {
-    setLanguageFilter(languageFilter.concat([l]));
-  };
-
-  const quitLanguage = (l: Language) => {
-    setLanguageFilter(languageFilter.filter(lan => lan !== l));
-  };
-
-  const toggleLanguage = (l: Language) => {
-    if(languageFilter.includes(l)){
-      quitLanguage(l);
-    }
-    else{
-      addLanguage(l);
-    }
-  }
-
-  const allLanguages = () => {
-    setLanguageFilter(languages);
-  }
-
-  const noLanguages = () => {
-    setLanguageFilter([]);
-  }
 
   return (
     <>
-      <div className="text-end mb-4">
-        <select
-          value={language}
-          onChange={(e) => setLanguage(e.target.value)}
-          className="bg-white dark:bg-[#242424] rounded"
-        >
-          {Array.from(trLanguages).map(([key, value], index) => (
-            <option value={key} key={index}>
-              {value}
-            </option>
-          ))}
-        </select>
+      <NavBar language={language} setLanguage={setLanguage}/>
+      <div className="flex sm:flex-row flex-col sm:items-center sm:justify-around w-full">
+        <h1 className="align-middle sm:w-1/5 w-full">Luciano Hernández</h1>
+        <div className="flex flex-col text-justify my-4 w-full sm:w-3/5">
+          <h2 className="text-center text-2xl font-bold py-5 w-full">{codeText("stt04", language)}</h2>
+          <p>{codeText("prg00", language)}</p>
+        </div>
       </div>
-      <h1>{codeText("ttl00", language)}</h1>
-      <h2 className="text-start text-2xl font-bold py-5">{codeText("stt00", language)}</h2>
-      <PersonalLink.Table
-        language={language}
-        links={links}
-        height={24}
-        tdClassName="text-start py-0.5 pr-2"
-      />
-      <h2 className="text-start text-2xl font-bold py-5">{codeText("stt01", language)}</h2>
-      <div className="flex flex-row">
-        <button onClick={allLanguages} className="mr-2 bg-green-500">{codeText("btn00", language)}</button>
-        <button onClick={noLanguages} className="bg-red-500">{codeText("btn01", language)}</button>
+      <div className="flex flex-col sm:flex-row w-full sm:justify-around">
+        <div className="flex flex-col">
+          <h2 className="text-center text-2xl font-bold py-5">{codeText("stt00", language)}</h2>
+          <PersonalLink.Table
+            language={language}
+            links={links}
+            height={24}
+            tdClassName="text-start py-0.5 pr-2"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-center text-2xl font-bold py-5">{codeText("stt03", language)}</h2>
+          <Category.Table
+            categories={categories}
+            href={c => `/Portfolio/Category?cat=${c.id}`}
+          />
+        </div>
+        <div className="flex flex-col sm:w-2/7">
+          <h2 className="text-center w-full text-2xl font-bold py-5">{codeText("stt01", language)}</h2>
+          <Language.List
+            className="flex flex-wrap justify-center"
+            href={l => `/Portfolio/Language?lan=${l.name}`}
+            languages={languages}
+            buttonClassName="flex m-1 justify-center p-2.5"
+            size={30}
+          />
+        </div>
       </div>
-      <Language.List
-        className="flex flex-wrap"
-        languages={languages}
-        onClick={toggleLanguage}
-        buttonClassName={l => `mr-2 mt-2 bg-${languageFilter.includes(l) ? "green" : "red" }-500`}
-        height={30}
-      />
-      <h2 className="text-start text-2xl font-bold py-5">{codeText("stt02", language)}</h2>
-      <Project.Table
-        language={language}
-        projects={projects}
-        languageFilter={languageFilter}
-        height={50}
-        lanHeight={20}
-        className="border-collapse"
-        thClassName="border border-solid px-2"
-        tdClassName="border border-solid p-2.5"
-      />
     </>
   )
 }
