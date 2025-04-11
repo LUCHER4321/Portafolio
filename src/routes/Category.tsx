e254fe4fd5244a3849f6eb8cb7204b050957aa64
LUCHER4321 <lucher4321@gmail.com>
2025-04-10 20:04:22 -0400

    Update Category.tsx

--- src/routes/Category.tsx
+++ src/routes/Category.tsx
@@ -1,33 +1,20 @@
 import { useState } from "react"
-import { useSearchParams } from "react-router-dom";
 import { NavBar } from "../components/NavBar";
-import { Project } from "../classes/Project";
-import { projects } from "../data/projects";
-import { codeText } from "../functions/translate";
 import { categories } from "../data/categories";
-import { Category } from "../classes/Category";
+import { ProjectsPage } from "./Projects";
 
 export const CategoryPage = () => {
     const [language, setLanguage] = useState("spanish");
-    const [searchParams] = useSearchParams();
-    const cat = searchParams.get("cat") || "";
-    const category = categories.find(c => c.id === cat) ?? new Category(cat, cat);
     return (
         <>
             <NavBar language={language} setLanguage={setLanguage}/>
-            <h1>{codeText("ttl02", language, [category.name.get(language) ?? ""])}</h1>
-            <div className="flex justify-center mt-4">
-                <Project.Table
-                    projects={projects.filter(p => p.categories.includes(category))}
-                    language={language}
-                    height={50}
-                    hoverHeight={55}
-                    lanSize={20}
-                    className="border-collapse"
-                    thClassName="border border-solid px-2"
-                    tdClassName="border border-solid p-2.5"
-                />
-            </div>
+            <ProjectsPage
+                paramName="cat"
+                titleCode="ttl02"
+                titleParam={cat => categories.find(c => c.id === cat)?.name.get(language) ?? ""}
+                projectFilter={p => p.categories.map(c => c.id)}
+                language={language}
+            />
         </>
     )
 }
