b9502d74f4ed7a2993e432f7ab37ac9e2ef41dd9
LUCHER4321 <lucher4321@gmail.com>
2025-04-11 00:02:57 -0400

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,7 +24,7 @@ export const NavBar = ({language, setLanguage}: NavBarProps) => {
     return (
         <>
             {projects && <button className="flex opacity-0 fixed top-0 bottom-0 right-0 left-0" onClick={switchProjects}/>}
-            <div className="flex fixed justify-between mb-4 w-full px-10 bg-[#6E17A2]/50 sm:bg-[#27273E]/50 top-0 right-0 h-15">
+            <div className="flex fixed justify-between mb-4 px-10 bg-[#27273E]/50 top-0 right-0 left-0 h-15">
                 <div className="flex flex-row">
                     <ImageLink
                         link="/Portafolio/"
@@ -34,17 +34,19 @@ export const NavBar = ({language, setLanguage}: NavBarProps) => {
                         className="mr-2 image-link-container"
                         blank={false}
                     />
-                    {links.filter(l => ["LinkedIn", "GitHub"].includes([...l.name.values()][0])).map((link, index) => (
-                        <ImageLink
-                            key={index}
-                            link={link.link}
-                            image={link.logo}
-                            height={25}
-                            hoverHeight={32}
-                            className="mx-2 image-link-container"
-                            alt={link.name.get(language)}
-                        />
-                    ))}
+                    <div className="hidden sm:flex">
+                        {links.filter(l => ["LinkedIn", "GitHub"].includes([...l.name.values()][0])).map((link, index) => (
+                            <ImageLink
+                                key={index}
+                                link={link.link}
+                                image={link.logo}
+                                height={25}
+                                hoverHeight={32}
+                                className="mx-2 image-link-container"
+                                alt={link.name.get(language)}
+                            />
+                        ))}
+                    </div>
                     <div className={"flex flex-col rounded" + ( projects ? " bg-[#7616AD]/75" : "")}>
                         <button className="bg-white/0! mt-1.5 text-start" onClick={switchProjects}>
                             {codeText("stt02", language)}
@@ -96,6 +98,19 @@ export const NavBar = ({language, setLanguage}: NavBarProps) => {
                         </option>
                     ))}
                 </select>
+            </div>
+            <div className="fixed flex sm:hidden bottom-0 right-0 left-0 justify-start mt-4 px-10 bg-[#27273E]/50 h-15">
+                {links.filter(l => ["LinkedIn", "GitHub"].includes([...l.name.values()][0])).map((link, index) => (
+                    <ImageLink
+                        key={index}
+                        link={link.link}
+                        image={link.logo}
+                        height={25}
+                        hoverHeight={32}
+                        className="mx-2 image-link-container"
+                        alt={link.name.get(language)}
+                    />
+                ))}
             </div>
             <div className="h-10"/>
         </>
