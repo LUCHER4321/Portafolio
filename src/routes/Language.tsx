33f4b7175ae6ca2154ac48b8f068f921e146e7b4
LUCHER4321 <lucher4321@gmail.com>
2025-04-10 20:04:24 -0400

    Update Language.tsx

--- src/routes/Language.tsx
+++ src/routes/Language.tsx
@@ -1,27 +1,18 @@
-import { useSearchParams } from "react-router-dom";
-import { Project } from "../classes/Project";
-import { projects } from "../data/projects";
 import { useState } from "react";
-import { codeText } from "../functions/translate";
 import { NavBar } from "../components/NavBar";
+import { ProjectsPage } from "./Projects";
 
 export const LanguagePage = () => {
     const [language, setLanguage] = useState("spanish");
-    const [searchParams] = useSearchParams();
-    const lan = searchParams.get("lan") || "";
     return (
         <>
             <NavBar language={language} setLanguage={setLanguage}/>
-            <h1>{codeText("ttl01", language, [lan])}</h1>
-            <div className="flex justify-center mt-4">
-                <Project.Table
-                    projects={projects.filter(p => p.languages.includes(lan))}
-                    language={language}
-                    className="border-collapse"
-                    thClassName="border border-solid px-2"
-                    tdClassName="border border-solid p-2.5"
-                />
-            </div>
+            <ProjectsPage
+                paramName="lan"
+                titleCode="ttl01"
+                projectFilter={p => p.languages.map(l => l.name)}
+                language={language}
+            />
         </>
     )
 };
