05515edd1859c7b4af88a4b176b5ab960fda47cf
LUCHER4321 <lucher4321@gmail.com>
2025-05-12 20:43:27 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LanguagePage } from './routes/Language.tsx'
 import { CategoryPage } from './routes/Category.tsx'
 import { LanguageUpdate } from './routes/Update/Language.tsx'
 import { CategoryUpdate } from './routes/Update/Category.tsx'
+import { ProjectUpdate } from './routes/Update/Project.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Route path="/Portafolio">
-        <Route path="/" element={<App/>}/>
-        <Route path="/Language/:lan" element={<LanguagePage/>}/>
-        <Route path="/Category/:cat" element={<CategoryPage/>}/>
-        <Route path="/Update">
-          <Route path="/Language" element={<LanguageUpdate/>}/>
-          <Route path="/Category" element={<CategoryUpdate/>}/>
-        </Route>
-      </Route>
+      <Routes>
+        <Route path="/Portafolio" element={<App/>}/>
+        <Route path="/Portafolio/Language/:lan" element={<LanguagePage/>}/>
+        <Route path="/Portafolio/Category/:cat" element={<CategoryPage/>}/>
+        <Route path="/Portafolio/Update/Language" element={<LanguageUpdate/>}/>
+        <Route path="/Portafolio/Update/Category" element={<CategoryUpdate/>}/>
+        <Route path="/Portafolio/Update/Project" element={<ProjectUpdate/>}/>
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
