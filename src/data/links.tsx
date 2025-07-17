6885eb5025a3e77d575b66bd3ca86e326d904ad2
LUCHER4321 <lucher4321@gmail.com>
2025-07-16 23:34:44 -0400

    Update links.tsx

--- src/data/links.tsx
+++ src/data/links.tsx
@@ -2,8 +2,8 @@ import { PersonalLink } from "../classes/PersonalLink";
 
 export const links = [
     ["lan:LinkedIn", "https://www.linkedin.com/in/luciano-hern%C3%A1ndez-76b8b9257/", "https://img.logo.dev/linkedin.com?token=pk_VXzZR_o_QTelazRSvSRkNw&format=png&retina=true"],
-    ["spanish:Correo;english:Mail", "mailto:lhernandezr@udd.cl", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg/1200px-Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg.png"],
     ["lan:GitHub", "https://github.com/LUCHER4321", "https://img.logo.dev/github.com?token=pk_VXzZR_o_QTelazRSvSRkNw&format=png"],
+    ["spanish:Correo;english:Mail", "mailto:lhernandezr@udd.cl", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg/1200px-Microsoft_Office_Outlook_%282018%E2%80%93present%29.svg.png"],
     ["spanish:Curriculum en Español;english:Resume in Spanish", "https://drive.google.com/file/d/1Y1aUuTzrS1YxRh1zetu38unMMlBcjnJz/view?usp=sharing", "https://img.logo.dev/pdfsimpli.com?token=pk_VXzZR_o_QTelazRSvSRkNw&format=png&retina=true"],
     ["spanish:Curriculum en Inglés;english:Resume in English", "https://drive.google.com/file/d/1AWzMUdG7-dOwoMyu5dfKeI3KtCGc90zV/view?usp=sharing", "https://img.logo.dev/pdfsimpli.com?token=pk_VXzZR_o_QTelazRSvSRkNw&format=png&retina=true"],
     ["spanish:Certificados;english:Certificates", "https://drive.google.com/drive/folders/17vUsvy5fZZEV04eHkKM0vIwOROTxGUQV?usp=drive_link", "https://upload.wikimedia.org/wikipedia/commons/thumb/archive/8/8a/20191115145934%21Google_Drive_logo_%282014-2020%29.svg/120px-Google_Drive_logo_%282014-2020%29.svg.png"]
