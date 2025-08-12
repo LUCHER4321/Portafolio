5d33e57c889b041cf77a348cc6208e3c65984018
LUCHER4321 <lucher4321@gmail.com>
2025-08-12 15:20:30 -0400

    Update ContactFrom.tsx

--- src/components/ContactFrom.tsx
+++ src/components/ContactFrom.tsx
@@ -7,7 +7,7 @@ export const ContactForm = ({language}: {language: string}) => {
     const textAreaClass = `${baseClass} row-span-4 sm:row-start-1 sm:col-start-4! col-span-1 sm:col-span-4`;
     return (
         <div className="w-full flex flex-col items-center">
-            <h2>{codeText("stt05", language)}</h2>
+            <h2 className="text-white">{codeText("stt05", language)}</h2>
             <form className="grid grid-cols-1 grid-flow-row sm:grid-rows-5 sm:grid-cols-7 sm:w-11/24 mt-4 gap-4" action={`https://formsubmit.co/${EMAIL_CODE}`} method="POST">
                 <input type="text" className={inputClass} placeholder={codeText("frm00", language) + "*"} name="name" required/>
                 <input type="text" className={inputClass} placeholder={codeText("frm01", language)} name="company"/>
